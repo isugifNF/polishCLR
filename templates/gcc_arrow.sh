7dcf89776893adb52292e3ea48ecad2a208d780a
j23414 <jenchang@iastate.edu>
2021-07-24 01:44:09 -0500

    fix gcpp window typo

--- templates/gcc_arrow.sh
+++ templates/gcc_arrow.sh
@@ -2,6 +2,6 @@
 PROC=\$(((`nproc`-1)*3/4+1))
 ${gcpp_app} --algorithm=arrow \
   -x 10 -X 120 -q 0 \
-  -j \${PROC} -w \"$window\" \
+  -j \${PROC} -w "$window" \
   -r ${assembly_fasta} ${pacbio_bam} \
   -o ${window.replace(':','_').replace('|','_')}.vcf,${window.replace(':','_').replace('|','_')}.fasta
