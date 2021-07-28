f3ce2054103bba3a4135d6a0ed85514cbc5e81a4
Astahlke <amanda.stahlke@ceres.scinet.local>
2021-07-28 18:13:40 -0500

    header for merqury

--- templates/merquryqv.sh
+++ templates/merquryqv.sh
@@ -1,2 +1,6 @@
 #! /usr/bin/env bash
+
+printf "======================================================="
+printf "uniq kmers in asm | kmers in both asm and reads | QV | Error rate"
+printf "======================================================="
 ${merqury_sh} $illumina_db $assembly_fasta ${assembly_fasta.simpleName}
