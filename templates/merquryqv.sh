f1421f8c21be1234fa124183211b03694b5bbb3e
Astahlke <amanda.stahlke@ceres.scinet.local>
2021-07-29 09:40:04 -0500

    adding print statements to merqury

--- templates/merquryqv.sh
+++ templates/merquryqv.sh
@@ -4,3 +4,7 @@ printf "======================================================="
 printf "uniq kmers in asm | kmers in both asm and reads | QV | Error rate"
 printf "======================================================="
 ${merqury_sh} $illumina_db $assembly_fasta ${assembly_fasta.simpleName}
+printf "=======================================================" > ${assembly_fasta.simpleName}_qv.txt
+printf "uniq kmers in asm | kmers in both asm and reads | QV | Error rate" >> ${assembly_fasta.simpleName}_qv.txt
+printf "=======================================================" >> ${assembly_fasta.simpleName}_qv.txt
+cat  ${assembly_fasta.simpleName}.qv >> ${assembly_fasta.simpleName}_qv.txt
