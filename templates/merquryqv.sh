27dd86524fb40ef1d4096e0c9eb02d8f5ad73347
j23414 <jenchang@iastate.edu>
2021-09-07 10:35:51 -0500

    update

--- templates/merquryqv.sh
+++ templates/merquryqv.sh
@@ -1,12 +1,12 @@
 #! /usr/bin/env bash
 
-printf "======================================================="
-printf "uniq kmers in asm | kmers in both asm and reads | QV | Error rate"
-printf "======================================================="
+printf "======================================================= \n"
+printf "uniq kmers in asm | kmers in both asm and reads | QV | Error rate \n"
+printf "======================================================= \n"
 ${merqury_sh} $illumina_db $assembly_fasta ${assembly_fasta.simpleName}
-printf "=======================================================" > ${assembly_fasta.simpleName}_qv.txt
-printf "uniq kmers in asm | kmers in both asm and reads | QV | Error rate" >> ${assembly_fasta.simpleName}_qv.txt
-printf "=======================================================" >> ${assembly_fasta.simpleName}_qv.txt
+printf "======================================================= \n" > ${assembly_fasta.simpleName}_qv.txt
+printf "uniq kmers in asm | kmers in both asm and reads | QV | Error rate \n" >> ${assembly_fasta.simpleName}_qv.txt
+printf "======================================================= \n" >> ${assembly_fasta.simpleName}_qv.txt
 cat  ${assembly_fasta.simpleName}.qv >> ${assembly_fasta.simpleName}_qv.txt
 
 # == Get single QV value
