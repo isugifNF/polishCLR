7556bf787fd155dad3c53e2604b61069afb6f994
j23414 <jenchang@iastate.edu>
2021-09-09 22:43:24 -0500

    use purge dups script from Astahlke

--- templates/purge_dups.sh
+++ templates/purge_dups.sh
@@ -1,55 +1,66 @@
 #! /usr/bin/env bash
 # genome_fasta=genome assembly file (cns_p_ctg.fasta)
-# haplotype_fasta=haplotype genome (cns_h_ctg.fasta)
+# haplo_fasta=haplotype genome (cns_h_ctg.fasta)
 # pacbio_reads=*subreads.fasta
 
 PROC=\$((`nproc`))
 
-${samtools_app} faidx ${genome_fasta}
-${minimap2_app} -xmap-pb -t \${PROC} ${genome_fasta} ${pacbio_reads} |\
-  ${gzip_app} -c - > p_mapping.paf.gz
+${samtools_app} faidx ${primary_assembly}
+
+${minimap2_app} -xmap-pb -t \${PROC}  ${primary_assembly} ${pacbio_reads} | \
+  ${gzip_app} -c -  > p_mapping.paf.gz
 
 ${pbcstat_app} p_mapping.paf.gz
-
 ${hist_plot_py} PB.stat primary_hist
 
-${calcuts_app} PB.stat > p_cutoffs 2> p_calcuts.log
-${split_fa} p_genome.fasta > p_genome.fasta.split
+${calcuts_app} PB.stat > p_cutoffs 2> p_calcuts.log 
 
-${minimap2_app} -xasm5 -DP -t \$PROC p_genome.fasta.split p_genome.fasta.split |\
-  ${gzip_app} -c - > p_genome.fasta.self.paf.gz
+${split_fa_app} ${primary_assembly} > ${primary_assembly}.split
 
-${purge_dups_app} -2 -T p_cufoffs -c PB.base.cov p_genome.fasta.split.self.paf.gz > p_dups.bed 2> p_purge_dups.log 
+${minimap2_app} -xasm5 -DP -t \${PROC} ${primary_assembly}.split ${primary_assembly}.split | gzip -c - > ${primary_assembly}.split.self.paf.gz
 
-## -e flag to only remove haplotypic regions at the ends of contigs (repeat of above)
-${get_seqs_app} -e p_dups.bed p_genome.fasta -p primary
+${purge_dups_app} -2 -T p_cufoffs -c PB.base.cov ${primary_assembly}.split.self.paf.gz > p_dups.bed 2> p_purge_dups.log
 
-## do the same with the "alternate" haps after cating 
-${samtools_app} faidx ${haplotype_fasta}
+## -e flag to only remove haplotypic regions at the ends of contigs
+${get_seqs_app} -e p_dups.bed ${primary_assembly} -p primary
 
-${minimap2_app} -xmap-pb -t 40 ${haplotype_fasta} ${pacbio_reads} |\
- ${gzip_app} -c -  > h_mapping.paf.gz
+echo "Purged primary, onto the alternate"
+
+## do the same with the "alternate" haps after cating
+cat primary.hap.fa  ${haplo_fasta} >  h_${haplo_fasta}
+
+${samtools_app} faidx h_${haplo_fasta}
+
+${minimap2_app} -xmap-pb -t \${PROC}  h_${haplo_fasta} ${pacbio_reads} |\
+  ${gzip_app} -c -  > h_mapping.paf.gz
 
 ${pbcstat_app} h_mapping.paf.gz
 
 ${calcuts_app} PB.stat > h_cutoffs 2> h_calcuts.log
 
-${split_fa_app} ${haplotype_fasta} > h_genome.fasta.split
+${split_fa_app} h_${haplo_fasta} > h_${haplo_fasta}.split
 
-${minimap2_app} -xasm5 -DP -t 40 h_genome.fasta.split h_genome.fasta.split | gzip -c - > h_genome.fasta.split.self.paf.gz
+${minimap2_app} -xasm5 -DP -t \${PROC} h_${haplo_fasta}.split h_${haplo_fasta}.split |\
+  ${gzip_app} -c - > h_${haplo_fasta}.split.self.paf.gz
 
-${purge_dups_app} -2 -T h_cufoffs -c PB.base.cov h_genome.fasta.split.self.paf.gz > h_dups.bed 2> h_purge_dups.log
+${purge_dups_app} -2 -T h_cufoffs -c PB.base.cov h_${haplo_fasta}.split.self.paf.gz > h_dups.bed 2> h_purge_dups.log
 
-${get_seqs_app} -e h_dups.bed h_genome.fasta -p haps
+${get_seqs_app} -e h_dups.bed h_${haplo_fasta} -p haps
 
-# after done, merge both
+#echo "Purged alternate, running bbtools stats.sh on each assembly"
+#module load bbtools
+#export PATH="/project/ag100pest/software/bbmap/:$PATH"
+#stats.sh -Xmx2048m primary.purged.fa > primary.purged.fa.stats
+#stats.sh -Xmx2048m primary.hap.fa > primary.hap.fa.stats
+#stats.sh -Xmx2048m haps.purged.fa > haps.purged.fa.stats
+#stats.sh -Xmx2048m haps.hap.fa > haps.hap.fa.stats
+## Instead of the above bbstats, kick off assemblystats nextflow. BUSCO is the most useful stat here.
 
-stats.sh -Xmx2048m primary.purged.fa > primary.purged.fa.stats
-
-stats.sh -Xmx2048m primary.hap.fa > primary.hap.fa.stats
-
-stats.sh -Xmx2048m haps.purged.fa > haps.purged.fa.stats
-
-stats.sh -Xmx2048m haps.hap.fa > haps.hap.fa.stats
-
-
+## rename to play nicely with nextflow shortname 
+mv primary.hap.fa primary_hap.fa
+mv primary.purged.fa primary_purged.fa
+#ln -s ${primary_assembly} p_genome.fa
+#ln -s ../Falcon/4-polish/cns-output/cns_h_ctg.fasta cns_h_ctg.fa
+mv haps.hap.fa haps_hap.fa
+mv haps.purged.fa haps_purged.fa
+mv  h_${haplo_fasta}  h_genome.fa
