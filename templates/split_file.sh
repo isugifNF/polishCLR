12c5f007bdc0c279300dcdf74dd90ef1e81c4a4b
j23414 <jenchang@iastate.edu>
2021-09-13 09:20:22 -0500

    format header for samtools faidx

--- templates/split_file.sh
+++ templates/split_file.sh
@@ -8,11 +8,11 @@
 # m_${genome_fasta}     # mitochondrial assembly
 
 ${samtools_app} faidx ${genome_fasta}
-grep ">pri_" ${genome_fasta} | cut -f1 > pri.list
+grep ">pri_" ${genome_fasta} | cut -f1 | sed 's/>//g' > pri.list
 ${samtools_app} faidx -r pri.list ${genome_fasta} > p_${genome_fasta}
 
-grep ">mit_" ${genome_fasta} | cut -f1 > mit.list
+grep ">mit_" ${genome_fasta} | cut -f1 | sed 's/>//g' > mit.list
 ${samtools_app} faidx -r mit.list ${genome_fasta} > m_${genome_fasta}
 
-grep ">alt_" ${genome_fasta} | cut -f1 > alt.list
+grep ">alt_" ${genome_fasta} | cut -f1 | sed 's/>//g' > alt.list
 ${samtools_app} faidx -r alt.list ${genome_fasta} > a_${genome_fasta}
