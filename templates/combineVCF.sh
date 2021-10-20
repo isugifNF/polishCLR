49cd3bca418f257b4762eda6bee9ffe8b648a058
Astahlke <amandastahlke@gmail.com>
2021-10-20 09:46:41 -0500

    replaced $i with $outdir

--- templates/combineVCF.sh
+++ templates/combineVCF.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-OUTVCF=${i}_consensus.vcf
+OUTVCF=${outdir}_consensus.vcf
 
 # Merge by sections (1) headers up to contig, (2) all contig headers, (3) headers post contigs, (4) snp data
 cat ${vcfs.get(0)} | sed -n '1,/##reference=/'p > \$OUTVCF
@@ -8,11 +8,3 @@ cat ${vcfs} | grep -h "##contig=" >> \$OUTVCF
 cat ${vcfs.get(0)} | sed -n '/##INFO=/,/#CHROM/'p >> \$OUTVCF
 cat ${vcfs} | grep -hv "#" >> \$OUTVCF
 
-# === The grep statements only pull one contig header
-#cat ${vcfs.get(0)} | grep "^#" > ${i}_consensus.vcf
-#cat ${vcfs} | grep -v "^#" >> ${i}_consensus.vcf
-# === Was going to use the following instead, but hit a duplicate sample name error
-#PROC=\$((`nproc`))
-#${parallel_app} -j \$PROC "bcftools view -Oz {1} > {1}.gz" ::: ${vcfs}
-#${parallel_app} -j \$PROC "bcftools index {1}.gz" ::: ${vcfs}
-#${bcftools_app} merge --force-samples *vcf.gz > ${i}_consensus.vcf
--- templates/vcf_to_fasta.sh
+++ templates/vcf_to_fasta.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 ${bcftools_app} view -Oz ${vcf} > ${vcf}.gz
 ${bcftools_app} index ${vcf}.gz
-${bcftools_app} consensus ${vcf}.gz -f ${genome_fasta} -Hla > ${i}_consensus.fasta
+${bcftools_app} consensus ${vcf}.gz -f ${genome_fasta} -Hla > ${outdir}_consensus.fasta
